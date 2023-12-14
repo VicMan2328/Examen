532d922b8d77c91766358b1b6762885a3151e82d
VicMan2328 <153864725+VicMan2328@users.noreply.github.com>
2023-12-14 14:26:28 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+
+
+<html>
+<head>
+<title>Problema</title>
+</head>
+
+<body>
+<?php
+function retornarColorHexa($rojo,$verde,$azul)
+{
+    $color=sprintf("#%02X%02X%02X",$rojo,$verde,$azul);
+    return $color;
+}
+?>
+
+<table>
+
+<tr>
+
+<td bgcolor="#33FF83">VICTOR MANUEL</td>
+<td bgcolor="#E86A07">OLVERA URQUIDEZ</td>
+<td bgcolor="#5911BA">21090143</td> 
+
+</tr>
+</table>
+</body>
+</html>
